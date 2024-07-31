459aebeb716c59aafacf8f9b56e561c1cfdc4ffe
nadiannis <annisanadianeyla@gmail.com>
2024-07-31 14:52:40 +0700

    fix: modify route path

--- cmd/routes.go
+++ cmd/routes.go
@@ -13,21 +13,21 @@ func (app *application) routes() http.Handler {
 		c.String(http.StatusOK, "API is running on port %d\n", app.config.port)
 	})
 
-	r.GET("GET /api/customers", app.handlers.Customers.GetAll)
-	r.POST("POST /api/customers", app.handlers.Customers.Add)
-	r.GET("GET /api/customers/{id}", app.handlers.Customers.GetByID)
-	r.PATCH("PATCH /api/customers/{id}/balances", app.handlers.Customers.AddBalance)
+	r.GET("/api/customers", app.handlers.Customers.GetAll)
+	r.POST("/api/customers", app.handlers.Customers.Add)
+	r.GET("/api/customers/{id}", app.handlers.Customers.GetByID)
+	r.PATCH("/api/customers/{id}/balances", app.handlers.Customers.AddBalance)
 
-	r.GET("GET /api/events", app.handlers.Events.GetAll)
-	r.GET("GET /api/events/{id}", app.handlers.Events.GetByID)
+	r.GET("/api/events", app.handlers.Events.GetAll)
+	r.GET("/api/events/{id}", app.handlers.Events.GetByID)
 
-	r.GET("GET /api/tickets", app.handlers.Tickets.GetAll)
-	r.GET("GET /api/tickets/{id}", app.handlers.Tickets.GetByID)
-	r.PATCH("PATCH /api/tickets/{id}/quantities", app.handlers.Tickets.AddQuantity) // Intended solely for concurrency testing purpose
+	r.GET("/api/tickets", app.handlers.Tickets.GetAll)
+	r.GET("/api/tickets/{id}", app.handlers.Tickets.GetByID)
+	r.PATCH("/api/tickets/{id}/quantities", app.handlers.Tickets.AddQuantity) // Intended solely for concurrency testing purpose
 
-	r.GET("GET /api/orders", app.handlers.Orders.GetAll)
-	r.POST("POST /api/orders", app.handlers.Orders.Add)
-	r.DELETE("DELETE /api/orders", app.handlers.Orders.DeleteAll) // Intended solely for concurrency testing purpose
+	r.GET("/api/orders", app.handlers.Orders.GetAll)
+	r.POST("/api/orders", app.handlers.Orders.Add)
+	r.DELETE("/api/orders", app.handlers.Orders.DeleteAll) // Intended solely for concurrency testing purpose
 
 	return requestLogger(r)
 }
